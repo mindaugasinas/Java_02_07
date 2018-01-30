d5c93249926cecdbcaac0f2dcbffae4165857eeb
mindaugasinas <sinkeviciusm@gmail.com>
2018-01-30 16:11:21 +0200

    Septinta uzduotis (projekto pradzia)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Iveskite savo ugi");
+
+        float Ugis = sc.nextFloat();
+
+        System.out.println("Iveskite savo svori");
+
+        float Svoris = sc.nextFloat();
+
+        System.out.println("Jusu KMI yra: " + KMI(Ugis, Svoris));
+
+    }
+
+    public static float KMI (float Ugis, float Svoris) {
+        return Svoris/(Ugis*Ugis);
     }
 }
